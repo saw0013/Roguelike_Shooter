bb664337df7a2765c0de437dfbaf7ce59d8c08c4
Roman <anomal3@yandex.ru>
2023-03-22 22:10:11 +0400

    Финальный коммит!
    Сервер чата перенесён в паблик в отдельный репозиторий...
    https://github.com/anomal3/ServerGameChatSignalR/releases

--- ServersASPNET/ChatAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
